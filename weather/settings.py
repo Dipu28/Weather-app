8e2c4953b480bebf69c4aecd5d461faaa67177b5
Dipu28 <dipanshusinghlove@gmail.com>
2020-10-03 17:44:00 +0530

    'Deploy'

--- weather/settings.py
+++ weather/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7w@9f!-xh%i(-^b6$cp^4x%7lzba36z*h52i@09&9wj^5&&#_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['weather-apo-1.herokuapp.com','127.0.0.1:']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'weather.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
