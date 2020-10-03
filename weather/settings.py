9c4fc616327df4fbc769696729e5a6a5120e38b2
Dipu28 <dipanshusinghlove@gmail.com>
2020-10-03 18:01:15 +0530

    'whitenoise'

--- weather/settings.py
+++ weather/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,6 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = [
+    os.path.join(BASE_DIR,'staticfiles')
+]
+
 STATIC_URL = '/static/'
 
-heroku config:set DISABLE_COLLECTSTATIC=1
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
